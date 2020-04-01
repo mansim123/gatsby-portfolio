41430dd9959230c5a0b9833f3f3a45e2d5ee9c8d
Manuel Yemoh <manuelyemoh@gmail.com>
2020-04-01 16:36:57 +0100

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/index.scss"
 
 const IndexPage = () => (
   <Layout>
@@ -11,7 +13,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div className="nav__bar">
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
