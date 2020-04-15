b27d5666182dcb3dc687fe9a9d97b118aefdf223
Manuel Yemoh <manuelyemoh@gmail.com>
2020-04-15 15:22:17 +0100

    fixed resizing of canvas

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,49 +2,26 @@ import React from "react"
 import "./hero.scss"
 import avatar from "../images/avatar.jpg"
 import { TweenMax } from "gsap"
+import { ScrollTo } from "react-scroll-to"
+
+let drawInterval
 
 class Hero extends React.Component {
   constructor() {
     super()
-
     // reference to the animation
     this.myTween = null
     this.animateArrows = this.animateArrows.bind(this)
+    this.draw = this.draw.bind(this)
   }
 
-  componentDidMount() {
-    //drawing the characters
-    function draw() {
-      //Black BG for the canvas
-      //translucent BG to show trail
-      ctx.fillStyle = "rgba(0, 0, 0, 0.04)"
-      ctx.fillRect(0, 0, m.width, m.height)
-
-      ctx.fillStyle = "#6666ff" //blue text
-      ctx.font = font_size + "px arial"
-      //looping over drops
-      for (var i = 0; i < drops.length; i++) {
-        //a random chinese character to print
-        var text = matrix[Math.floor(Math.random() * matrix.length)]
-        //x = i*font_size, y = value of drops[i]*font_size
-        ctx.fillText(text, i * font_size, drops[i] * font_size)
-
-        //sending the drop back to the top randomly after it has crossed the screen
-        //adding a randomness to the reset to make the drops scattered on the Y axis
-        if (drops[i] * font_size > m.height && Math.random() > 0.975)
-          drops[i] = 0
-
-        //incrementing Y coordinate
-        drops[i]++
-      }
-    }
-
+  initDraw(w, h) {
     var m = document.getElementById("m")
     var ctx = m.getContext("2d")
 
     //making the canvas full screen
-    m.height = window.innerHeight
-    m.width = window.innerWidth
+    m.height = h
+    m.width = w
 
     //chinese characters - taken from the unicode charset
     var matrix = "バカマン"
@@ -58,8 +35,48 @@ class Hero extends React.Component {
     //x below is the x coordinate
     //1 = y co-ordinate of the drop(same for every drop initially)
     for (var x = 0; x < columns; x++) drops[x] = 1
+    var self = this
+    drawInterval = setInterval(function() {
+      self.draw(ctx, drops, matrix, m, font_size)
+    }, 45)
+  }
 
-    setInterval(draw, 35)
+  //drawing the characters
+  draw(ctx, drops, matrix, m, font_size) {
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)"
+    ctx.fillRect(0, 0, m.width, m.height)
+
+    ctx.fillStyle = "#6666ff" //blue text
+    ctx.font = font_size + "px arial"
+    //looping over drops
+    for (var i = 0; i < drops.length; i++) {
+      //a random chinese character to print
+      var text = matrix[Math.floor(Math.random() * matrix.length)]
+      //x = i*font_size, y = value of drops[i]*font_size
+      ctx.fillText(text, i * font_size, drops[i] * font_size)
+
+      //sending the drop back to the top randomly after it has crossed the screen
+      //adding a randomness to the reset to make the drops scattered on the Y axis
+      if (drops[i] * font_size > m.height && Math.random() > 0.975) drops[i] = 0
+
+      //incrementing Y coordinate
+      drops[i]++
+    }
+  }
+
+  updateDimensions() {
+    clearInterval(drawInterval)
+    this.initDraw(window.innerWidth, window.innerHeight)
+  }
+
+  componentDidMount() {
+    this.initDraw(window.innerWidth, window.innerHeight)
+    window.addEventListener("resize", this.updateDimensions.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this))
   }
 
   animateArrows() {
@@ -142,6 +159,18 @@ class Hero extends React.Component {
               /
             </span>
           </div>
+          {/* <ScrollTo>
+            {({ scroll }) => (
+              <a
+                onClick={() =>
+                  scroll({ ref: this.myRef, x: 20, y: 500, smooth: true })
+                }
+              >
+                Scroll to Bottom
+              </a>
+            )}
+          </ScrollTo>
+          <div ref={this.myRef}>My Element</div> */}
         </div>
       </section>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/index.scss"
@@ -13,9 +13,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div className="nav__bar">
-      <Image />
-    </div>
+    <div className="nav__bar">{/* <Image /> */}</div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
