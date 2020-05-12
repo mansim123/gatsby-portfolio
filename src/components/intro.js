a31ce9b06dacf84b85bcb42ce77677b265d8814f
Manuel Yemoh <m.yemoh@dewynters.com>
2020-05-12 12:30:55 +0100

    creating intro page

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../styles/intro.scss"
-import avatar from "../images/avatar.jpg"
+import profilePic from "../images/profilePic.jpg"
 import { TweenMax } from "gsap"
 import { ScrollTo } from "react-scroll-to"
 
@@ -21,6 +21,19 @@ class Intro extends React.Component {
         <div className="introMainSection">
           <h1>The Drive to Learn</h1>
           <h2>working with all the latest technlogies</h2>
+        </div>
+        <div className="flexContainer">
+          <div>
+            <img src={profilePic} alt=""></img>
+          </div>
+          <div>
+            <h3>
+              Manuel Yemoh
+            </h3>
+            <p>
+              BSc in Computer Game Design, with over 9 years of professional digital development experience and 4 years running a successful production studio. Working with some of the biggest agencies in London, specialising in HTML5 digital development, project management and consultation. Available for on-site or remote work.
+            </p>
+          </div> 
         </div>
       </section>
     )
